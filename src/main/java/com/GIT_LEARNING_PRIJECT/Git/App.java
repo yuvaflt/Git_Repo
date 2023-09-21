922cb7c0e56639e4507d0fd620f60e06b709cfa4
yuvaflt <feltanejuba97@gmail.com>
2023-09-20 20:08:14 -0400

    My Project

---
+++ src/main/java/com/GIT_LEARNING_PRIJECT/Git/App.java
@@ -0,0 +1,13 @@
+package com.GIT_LEARNING_PRIJECT.Git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/GIT_LEARNING_PRIJECT/Git/Git1.java
@@ -0,0 +1,10 @@
+package com.GIT_LEARNING_PRIJECT.Git;
+
+public class Git1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
